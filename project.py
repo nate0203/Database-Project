5b031dd6b9c50a06ae30a29c0032223d75cb8884
anishmalhotra <anishmalhotra97@gmail.com>
2017-12-06 20:31:58 -0500

    Create FG/Add Friends/View Post/Make comments all done

--- project.py
+++ project.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 conn = pymysql.connect(host='localhost',
                        user='root',
-                       password='',
+                       password='root',
                        db='pcs',
                        charset='utf8',
                        cursorclass=pymysql.cursors.DictCursor)
@@ -90,11 +90,24 @@ def home():
 	query = 'SELECT * FROM Content NATURAL LEFT JOIN Share WHERE username = %s ORDER BY timest DESC'
 	cursor.execute(query, (username))
 	data = cursor.fetchall()
+
+	query = 'SELECT group_name FROM FriendGroup WHERE username = %s'
+	cursor.execute(query, username)
+	data2 = cursor.fetchall()
+	temp = []
+	group_list = []
+
+	for item in data2:
+		temp.append(item.values())
+	for i in range(len(temp)):
+		group_list.append(temp[i][0])
+		print group_list[i]
+
 	cursor.close()
 	if error == None:
-		return render_template('home.html', username=username, posts=data)
+		return render_template('home.html', username=username, posts=data, group_list=group_list)
 	else:
-		return render_template('home.html', username=username, posts=data, error=error)
+		return render_template('home.html', username=username, posts=data, error=error, group_list=group_list)
 
 @app.route('/logout')
 def logout():
@@ -118,12 +131,22 @@ def post():
 		visibility = False
 		query = 'SELECT group_name FROM Member WHERE username = %s'
 		cursor.execute(query, username)
-		group_list = cursor.fetchall()
-		if group not in group_list:
-			error = 'This FriendGroup does not exist.'
-			session['error'] = error
-			cursor.close()
-			return redirect(url_for('home'))
+		data = cursor.fetchall()
+
+		temp = []
+		group_list = []
+
+		for item in data:
+			temp.append(item.values())
+		for i in range(len(temp)):
+			friend_list.append(temp[i][0])
+			print friend_list[i]
+
+		#if group not in group_list:
+			#error = 'This FriendGroup does not exist.'
+			#session['error'] = error
+			#cursor.close()
+			#return redirect(url_for('home'))
 			# need redirect to error page or home.html withall info
 
 	#false -> private, has a friendgroup associated with it
@@ -159,13 +182,202 @@ def post():
 	cursor.close()
 	return redirect(url_for('home'))
 
-@app.route('/edit_fg', methods=['GET', 'POST'])
-def edit_fg():
+@app.route('/post/content_id=<id>', methods=['GET', 'POST'])
+def view_post(id):
+	session['id'] = id
+	username = session['username']
+	cursor = conn.cursor()
+	query = 'SELECT * FROM Content NATURAL LEFT JOIN Share WHERE username = %s AND id = %s ORDER BY timest DESC'
+	cursor.execute(query, (username,id))
+	data = cursor.fetchall()
+	cursor.close()
+
+	cursor = conn.cursor()
+	query = 'SELECT username, comment_text, timest FROM Comment WHERE id = %s ORDER BY timest DESC'
+	cursor.execute(query, (id))
+	data2 = cursor.fetchall()
+	cursor.close()
+
+	return render_template('comments.html', post=data, comments=data2)
+
+@app.route('/comment', methods=['GET', 'POST'])
+def comment():
+	id = session['id']
+	username = session['username']
+	text = request.form['Comment']
+	cursor = conn.cursor()
+	ins = 'INSERT INTO Comment (id, username, comment_text) VALUES (%s,%s,%s)'
+	cursor.execute(ins, (id, username, text))
+	conn.commit()
+	cursor.close()
+	return redirect(url_for('view_post', id=id))
+
+@app.route('/friendgroup')
+def friendgroups():
 	return render_template('friendgroup.html')
 
-#@app.route('/create_fg', methods=['GET', 'POST'])
-#def create_fg():
+@app.route('/add_to_fg', methods=['GET', 'POST'])
+def add_to_fg():
+	username = session['username']
+	cursor = conn.cursor();
 	
+	query = 'SELECT username FROM Person WHERE username!=%s'
+	cursor.execute(query, (username))
+	data = cursor.fetchall()
+	query = 'SELECT group_name FROM FriendGroup WHERE username = %s'
+	cursor.execute(query, (username))
+	data2 = cursor.fetchall()
+	cursor.close()
+
+	temp = []
+	friend_list = []
+	group_list = []
+
+	for item in data:
+		temp.append(item.values())
+	for i in range(len(temp)):
+		friend_list.append(temp[i][0])
+		print friend_list[i]
+	temp[:] = []
+	for item in data2:
+		temp.append(item.values())
+	for i in range(len(temp)):
+		group_list.append(temp[i][0])
+
+	return render_template('edit_fg.html', friend_list=friend_list, group_list=group_list)
+
+@app.route('/insert_fg', methods=['GET', 'POST'])
+def insert_fg():
+	username = session['username']
+	group = request.form['group']
+	friend = request.form['friend']
+	cursor = conn.cursor()
+
+	query = 'SELECT username FROM Member WHERE group_name = %s AND username_creator = %s'
+	cursor.execute(query, (group, username))
+	data = cursor.fetchall()
+	query = 'SELECT group_name FROM FriendGroup WHERE username = %s'
+	cursor.execute(query, (username))
+	data2 = cursor.fetchall()
+	query = 'SELECT username FROM Person WHERE username!=%s'
+	cursor.execute(query, (username))
+	data3 = cursor.fetchall()
+
+	cursor.close()
+
+	cursor = conn.cursor()
+
+	temp = []
+	friend_list = []
+	group_list = []
+	total_fl = []
+
+	for item in data:
+		temp.append(item.values())
+	for i in range(len(temp)):
+		friend_list.append(temp[i][0])
+		print friend_list[i]
+	temp[:] = []
+	for item in data2:
+		temp.append(item.values())
+	for i in range(len(temp)):
+		group_list.append(temp[i][0])
+	temp[:] = []
+	for item in data3:
+		temp.append(item.values())
+	for i in range(len(temp)):
+		total_fl.append(temp[i][0])
+
+	if friend in friend_list:
+		err = friend + ' is already in ' + group
+		session['err'] = err
+		cursor.close()
+		return render_template('edit_fg.html', friend_list=total_fl, group_list=group_list, error=err)
+
+	ins = 'INSERT INTO Member (username, group_name, username_creator) VALUES (%s,%s,%s)'
+	cursor.execute(ins, (friend, group, username))
+	conn.commit()
+	cursor.close()
+
+	success = friend + ' was added to ' + group + '!'
+	session['success'] = success
+
+	return render_template('edit_fg.html', friend_list=total_fl, group_list=group_list, success=success)
+
+@app.route('/create_fg', methods=['GET', 'POST'])
+def create_fg():
+	username = session['username']
+	cursor = conn.cursor();
+	query = 'SELECT username FROM Person WHERE username!=%s'
+	cursor.execute(query, (username))
+	data = cursor.fetchall()
+	cursor.close()
+
+	temp = []
+	friend_list = []
+
+	for item in data:
+		temp.append(item.values())
+	for i in range(len(temp)):
+		friend_list.append(temp[i][0])
+		print friend_list[i]
+
+	return render_template('create_fg.html', friend_list=friend_list)
+
+@app.route('/new_fg', methods=['GET', 'POST'])
+def new_fg():
+	username = session['username']
+	group = request.form['newgroup']
+	friend_1 = request.form['friend1']
+	friend_2 = request.form['friend2']
+	description = request.form['description']
+	cursor = conn.cursor()
+
+	temp = []
+	group_list = []
+	friend_list = []
+
+	query = 'SELECT group_name FROM FriendGroup WHERE username = %s'
+	cursor.execute(query, username)
+	data = cursor.fetchall()
+
+	query = 'SELECT username FROM Person WHERE username!=%s'
+	cursor.execute(query, (username))
+	data2 = cursor.fetchall()
+
+	for item in data:
+		temp.append(item.values())
+	for i in range(len(temp)):
+		group_list.append(temp[i][0])
+		print group_list[i]
+	temp[:] = []
+	for item in data2:
+		temp.append(item.values())
+	for i in range(len(temp)):
+		friend_list.append(temp[i][0])
+		print friend_list[i]
+
+	if group in group_list:
+		err = 'This FriendGroup already exists.'
+		session['err'] = err
+		cursor.close()
+		return render_template('create_fg.html', friend_list=friend_list, error=err)
+	if friend_1 == friend_2:
+		err = 'Duplicate friends selected'
+		session['err'] = err
+		cursor.close()
+		return render_template('create_fg.html', friend_list=friend_list, error=err)
+
+	ins = 'INSERT INTO FriendGroup (group_name, username, description) VALUES (%s,%s,%s)'
+	cursor.execute(ins, (group, username, description))
+	conn.commit()
+	ins = 'INSERT INTO Member (username, group_name, username_creator) VALUES (%s,%s,%s)'
+	cursor.execute(ins, (friend_1, group, username))
+	conn.commit()
+	cursor.execute(ins, (friend_2, group, username))
+	conn.commit()
+	cursor.close()
+	return redirect(url_for('home'))
 
 app.secret_key = 'some key that you will never guess'
 #Run the app on localhost port 5000
